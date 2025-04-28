e6e168c513cda86a7627157a8445b886f6afdc14
Diogo Neiva Abreu <diogo.neiva-abreu@fyrstain.com>
2025-04-28 15:52:23 +0200

    SOC-319 | Reformate files

--- src/test/java/ca/uhn/fhir/jpa/starter/datamart/r5/DatamartTransformationTest.java
+++ src/test/java/ca/uhn/fhir/jpa/starter/datamart/r5/DatamartTransformationTest.java
@@ -25,71 +25,71 @@ import static org.mockito.Mockito.*;
 
 class DatamartTransformationTest {
 
-    private Repository repository;
-    private DatamartTransformation transformation;
+	private Repository repository;
+	private DatamartTransformation transformation;
 
-    @BeforeEach
-    void setUp() {
-        repository = mock(Repository.class);
-        transformation = new DatamartTransformation(repository);
-    }
+	@BeforeEach
+	void setUp() {
+		repository = mock(Repository.class);
+		transformation = new DatamartTransformation(repository);
+	}
 
-    @Test
-    void transformSuccess() {
-        ResearchStudy researchStudy = new ResearchStudy();
-        researchStudy.getPhase().addCoding()
-                .setSystem(ResearchStudyUtils.CUSTOM_PHASE_SYSTEM)
-                .setCode(ResearchStudyUtils.POST_DATAMART);
-        researchStudy.addExtension()
-                .setUrl(ResearchStudyUtils.EXT_URL)
-                .addExtension()
-                .setUrl(ResearchStudyUtils.EVAL_EXT_NAME)
-                .setValue(new Reference("List/List123"));
+	@Test
+	void transformSuccess() {
+		ResearchStudy researchStudy = new ResearchStudy();
+		researchStudy.getPhase().addCoding()
+			.setSystem(ResearchStudyUtils.CUSTOM_PHASE_SYSTEM)
+			.setCode(ResearchStudyUtils.POST_DATAMART);
+		researchStudy.addExtension()
+			.setUrl(ResearchStudyUtils.EXT_URL)
+			.addExtension()
+			.setUrl(ResearchStudyUtils.EVAL_EXT_NAME)
+			.setValue(new Reference("List/List123"));
 
-        StructureMap map = new StructureMap();
-        Bundle bundle = new Bundle();
-        bundle.addEntry().setResource(new ListResource());
+		StructureMap map = new StructureMap();
+		Bundle bundle = new Bundle();
+		bundle.addEntry().setResource(new ListResource());
 
-        when(repository.fhirContext()).thenReturn(FhirContext.forR5());
+		when(repository.fhirContext()).thenReturn(FhirContext.forR5());
 
-        try (MockedStatic<Repositories> repo = Mockito.mockStatic(Repositories.class)) {
-            repo.when(() -> Repositories.proxy(any(), anyBoolean(), (IBaseResource) any(), any(), any()))
-                    .thenReturn(repository);
-            when(repository.search(eq(Bundle.class), eq(ListResource.class), anyMap(), isNull())).thenReturn(bundle);
+		try (MockedStatic<Repositories> repo = Mockito.mockStatic(Repositories.class)) {
+			repo.when(() -> Repositories.proxy(any(), anyBoolean(), (IBaseResource) any(), any(), any()))
+				.thenReturn(repository);
+			when(repository.search(eq(Bundle.class), eq(ListResource.class), anyMap(), isNull())).thenReturn(bundle);
 
-            Binary expectedBinary = new Binary();
-            try (MockedStatic<DatamartTransformation> transformationMock = Mockito.mockStatic(DatamartTransformation.class)) {
-                when(repository.invoke(eq("transform"), any(Parameters.class), eq(Parameters.class), isNull()))
-                        .thenReturn(new Parameters().addParameter(new Parameters.ParametersParameterComponent().setResource(expectedBinary)));
+			Binary expectedBinary = new Binary();
+			try (MockedStatic<DatamartTransformation> transformationMock = Mockito.mockStatic(DatamartTransformation.class)) {
+				when(repository.invoke(eq("transform"), any(Parameters.class), eq(Parameters.class), isNull()))
+					.thenReturn(new Parameters().addParameter(new Parameters.ParametersParameterComponent().setResource(expectedBinary)));
 
-                Binary result = transformation.transform(researchStudy, map);
+				Binary result = transformation.transform(researchStudy, map);
 
-                assertNotNull(result);
-                assertSame(expectedBinary, result);
-                verify(repository).invoke(eq("transform"), any(Parameters.class), eq(Parameters.class), isNull());
-            }
-        }
-    }
+				assertNotNull(result);
+				assertSame(expectedBinary, result);
+				verify(repository).invoke(eq("transform"), any(Parameters.class), eq(Parameters.class), isNull());
+			}
+		}
+	}
 
-    @Test
-    void fetchDatamartBundleSuccess() {
-        String listId = "List123";
-        Bundle expectedBundle = new Bundle();
+	@Test
+	void fetchDatamartBundleSuccess() {
+		String listId = "List123";
+		Bundle expectedBundle = new Bundle();
 
-        try (MockedStatic<Repositories> repo = Mockito.mockStatic(Repositories.class)) {
-            repo.when(() -> Repositories.proxy(any(), anyBoolean(), (IBaseResource) any(), any(), any()))
-                    .thenReturn(repository);
-            when(repository.search(eq(Bundle.class), eq(ListResource.class), anyMap(), isNull())).thenReturn(expectedBundle);
+		try (MockedStatic<Repositories> repo = Mockito.mockStatic(Repositories.class)) {
+			repo.when(() -> Repositories.proxy(any(), anyBoolean(), (IBaseResource) any(), any(), any()))
+				.thenReturn(repository);
+			when(repository.search(eq(Bundle.class), eq(ListResource.class), anyMap(), isNull())).thenReturn(expectedBundle);
 
-            Bundle result = transformation.fetchDataMartBundle(listId);
+			Bundle result = transformation.fetchDataMartBundle(listId);
 
-            assertNotNull(result);
-            assertEquals(expectedBundle, result);
+			assertNotNull(result);
+			assertEquals(expectedBundle, result);
 
-            Map<String, List<IQueryParameterType>> expectedParams = new HashMap<>();
-            expectedParams.put("_id", Collections.singletonList(new StringParam(listId)));
-            expectedParams.put("_include", Collections.singletonList(new StringParam("List:item")));
-            verify(repository).search(eq(Bundle.class), eq(ListResource.class), eq(expectedParams), isNull());
-        }
-    }
+			Map<String, List<IQueryParameterType>> expectedParams = new HashMap<>();
+			expectedParams.put("_id", Collections.singletonList(new StringParam(listId)));
+			expectedParams.put("_include", Collections.singletonList(new StringParam("List:item")));
+			verify(repository).search(eq(Bundle.class), eq(ListResource.class), eq(expectedParams), isNull());
+		}
+	}
 }
