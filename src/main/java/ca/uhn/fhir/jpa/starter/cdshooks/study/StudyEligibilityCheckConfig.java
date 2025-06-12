1470bfdc4dcd90d190773a28a7bb1fccb7996241
OmarGuend <144893266+OmarGuend@users.noreply.github.com>
2025-06-12 10:45:42 +0200

    SOC-439 | add StudyEligibilityCheckConfig

--- src/main/java/ca/uhn/fhir/jpa/starter/cdshooks/study/StudyEligibilityCheckConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/cdshooks/study/StudyEligibilityCheckConfig.java
@@ -1,7 +1,6 @@
 package ca.uhn.fhir.jpa.starter.cdshooks.study;
 
-import ca.uhn.fhir.jpa.starter.cdshooks.CdsHooksServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import ca.uhn.fhir.jpa.starter.cdshooks.study.r5.StudyEligibilityCheckService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +9,10 @@ import java.util.List;
 
 @Configuration
 public class StudyEligibilityCheckConfig {
-
-	@Bean(name = "cdsServices")
-	public List<Object> cdsServices() {
-		List<Object> retVal = new ArrayList<>();
-		retVal.add(new StudyEligibilityCheckService());
-		return retVal;
-	}
+    @Bean(name = "cdsServices")
+    public List<Object> cdsServices() {
+        List<Object> retVal = new ArrayList<>();
+        retVal.add(new StudyEligibilityCheckService());
+        return retVal;
+    }
 }
